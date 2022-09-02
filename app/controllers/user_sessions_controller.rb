58575a50a8f55cc27822b59c9cf60983ce6e37ab
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-09-02 15:29:33 +0900

    [add]add before_action

--- app/controllers/user_sessions_controller.rb
+++ app/controllers/user_sessions_controller.rb
@@ -1,8 +1,10 @@
 class UserSessionsController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
   def new; end
 
   def create
-    @user = login(params[:email], [:password])
+    @user = login(params[:email], params[:password])
     if @user
       redirect_back_or_to root_path
     else
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,13 @@
 class UsersController < ApplicationController
+  skip_before_action :require_login, only: %i[new create]
+
   def new
     @user = User.new
   end
 
   def create
     @user = User.new(user_params)
-    if @user.save
+    if @user.save!
       redirect_to login_path
     else
       render :new
