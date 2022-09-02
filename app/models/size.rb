f0679456539384f0aba12b0a1e671f88e18f58aa
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-09-02 15:15:24 +0900

    [fix]fix validation and db

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,3 +1,7 @@
 class Size < ApplicationRecord
-  belongs_to :tops
+  belongs_to :user
+  has_many :tops, dependent: :destroy
+  has_many :bottoms, dependent: :destroy
+
+  validates :title, presence: true
 end
