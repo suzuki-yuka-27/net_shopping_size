dcd27b807247381bff6d2b989783684967f28e2a
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-09-07 21:07:55 +0900

    [fix]fix default value

--- app/models/bottom.rb
+++ app/models/bottom.rb
@@ -9,11 +9,11 @@ class Bottom < ApplicationRecord
   validates :thickness_of_thigh, presence: true
   validates :bottom_width, presence: true
 
-  attribute :waist, :float, default: 0
-  attribute :hip, :float, default: 0
-  attribute :rising_length, :float, default: 0
-  attribute :inseam, :float, default: 0
-  attribute :total_length, :float, default: 0
-  attribute :thickness_of_thigh, :float, default: 0
-  attribute :bottom_width, :float, default: 0
+  attribute :waist, :float, default: 0.0
+  attribute :hip, :float, default: 0.0
+  attribute :rising_length, :float, default: 0.0
+  attribute :inseam, :float, default: 0.0
+  attribute :total_length, :float, default: 0.0
+  attribute :thickness_of_thigh, :float, default: 0.0
+  attribute :bottom_width, :float, default: 0.0
 end
