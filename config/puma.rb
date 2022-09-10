8a8eb1de372123b642986887b9cce8743923c416
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-09-11 04:33:42 +0900

    [fix]fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -15,7 +15,7 @@ worker_timeout 3600 if ENV.fetch("RAILS_ENV", "development") == "development"
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-bind "unix:///var/www/test_app/tmp/sockets/puma.sock"
+bind "unix:///var/www/net_shopping_size/tmp/sockets/puma.sock"
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
