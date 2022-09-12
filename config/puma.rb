76e404ed668f81fd30390362f41fd5fc88f5b036
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-09-12 13:50:59 +0900

    [fix]fix puma.rb

--- config/puma.rb
+++ config/puma.rb
@@ -14,7 +14,7 @@ threads min_threads_count, max_threads_count
 
 # Specifies the `port` that Puma will listen on to receive requests; default is 3000.
 #
-bind "unix:///var/www/net_shopping_size/tmp/sockets/puma.sock"
+port        ENV.fetch("PORT") { 3000 }
 # Specifies the `environment` that Puma will run in.
 #
 environment ENV.fetch("RAILS_ENV") { "development" }
