c4a34457bb70176d37c7a691dece1f86e6c6729e
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-10-14 22:33:26 +0900

    [add/fix]add and fix files

---
+++ db/migrate/20221013064325_create_tops.rb
@@ -0,0 +1,16 @@
+class CreateTops < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tops do |t|
+      t.string :title, null: false
+      t.float :neck, null:false
+      t.float :shoulder_width, null: false
+      t.float :body_width, null: false
+      t.float :body_length, null: false
+      t.float :sleeve_length, null: false
+      t.float :sleeve_width, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
