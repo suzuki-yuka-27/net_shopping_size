9e8bf8636fb140943b27273b03489a67181bf86b
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-10-24 19:04:59 +0900

    [delete]delete title culumn

--- db/migrate/20221016061920_create_hats.rb
+++ db/migrate/20221016061920_create_hats.rb
@@ -1,7 +1,6 @@
 class CreateHats < ActiveRecord::Migration[7.0]
   def change
     create_table :hats do |t|
-      t.string :title, null: false
       t.float :head_circumference, null: false
       t.references :user, null: false, foreign_key: true
 
