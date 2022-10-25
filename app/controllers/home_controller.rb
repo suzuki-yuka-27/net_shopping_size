891ffac007ee1ffcdc09db303eea24f9c5cc1edc
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-10-25 16:25:46 +0900

    [add]add input data to home

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,3 +1,11 @@
 class HomeController < ApplicationController
-  def index; end
+  def index
+    @top = User.find(current_user.id).top
+    @bottom = User.find(current_user.id).bottom
+    @one_piece = User.find(current_user.id).one_piece
+    @outer = User.find(current_user.id).outer
+    @hat = User.find(current_user.id).hat
+    @ring = User.find(current_user.id).ring
+    @underwear = User.find(current_user.id).underwear
+  end
 end
