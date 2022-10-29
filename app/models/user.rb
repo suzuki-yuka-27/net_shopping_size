a519bc802c0a9cf463341aa6be03ba3ba3070d09
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-10-29 20:36:40 +0900

    [fix]fix validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,18 +1,13 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  has_many :tops, dependent: :destroy
-  has_many :bottoms, dependent: :destroy
-  has_many :hats, dependent: :destroy
-  has_many :rings, dependent: :destroy
-  has_many :one_pieces, dependent: :destroy
-  has_many :outers, dependent: :destroy
-  has_many :underwears, dependent: :destroy
+  has_one :top, dependent: :destroy
+  has_one :bottom, dependent: :destroy
+  has_one :hat, dependent: :destroy
+  has_one :ring, dependent: :destroy
+  has_one :one_piece, dependent: :destroy
+  has_one :outer, dependent: :destroy
+  has_one :underwear, dependent: :destroy
 
-  validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
-  validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
-  validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
-
-  validates :email, presence: true
   validates :line_user_id, presence: true, uniqueness: true
 end
