2555a6b0268e4761d1a3c8c2e99efce4df395cee
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-11-11 18:46:41 +0900

    [delete]delete culumn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_10_26_010850) do
+ActiveRecord::Schema[7.0].define(version: 2022_10_19_113233) do
   create_table "bottoms", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.float "waist", null: false
     t.float "hip", null: false
@@ -99,7 +99,6 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_010850) do
     t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "line_user_id", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
