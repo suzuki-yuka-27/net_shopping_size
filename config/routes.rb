96f2b0978719ad9aac3e9bfd3cff1efd045d969a
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-11-11 15:14:03 +0900

    [add]add sign up and login function

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root to: 'home#index'
 
+  get 'login', to: 'user_sessions#new'
+  post 'login', to: 'user_sessions#create'
+  delete 'logout', to: 'user_sessions#destroy'
+
+  resources :users, only: %i[new create]
 
   resource :top, only: %i[new create show edit update]
   resource :bottom, only: %i[new create show edit update]
--- db/schema.rb
+++ db/schema.rb
@@ -94,10 +94,13 @@ ActiveRecord::Schema[7.0].define(version: 2022_10_26_010850) do
   end
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email"
+    t.string "email", null: false
+    t.string "crypted_password"
+    t.string "salt"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "line_user_id", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "bottoms", "users"
