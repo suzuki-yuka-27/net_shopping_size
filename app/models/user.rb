2fcb4469de965cc038de8dfe94ce8ca3861e9e83
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-11-29 16:09:20 +0900

    [fix]fix user of rspec

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
+  validates :email, presence: true, uniqueness: true
+
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
   validates :password_confirmation, presence: true, if: -> { new_record? || changes[:crypted_password] }
