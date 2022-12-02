481a99daee472343399b9fd376f9dafa11d7eb3f
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-12-02 14:38:12 +0900

    [fix]fix email conditions

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   authenticates_with_sorcery!
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true
 
   validates :password, length: { minimum: 5 }, if: -> { new_record? || changes[:crypted_password] }
   validates :password, confirmation: true, if: -> { new_record? || changes[:crypted_password] }
