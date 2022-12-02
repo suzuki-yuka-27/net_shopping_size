888d1767961453c62254595dc1a6584d3652c7db
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2022-12-02 20:05:15 +0900

    [fix]fix factorybot define

--- spec/factories/top.rb
+++ spec/factories/top.rb
@@ -1,11 +1,11 @@
 FactoryBot.define do
   factory :top do
-    sequence(:neck, 20)
-    sequence(:shoulder_width, 20)
-    sequence(:body_width, 20)
-    sequence(:body_length, 20)
-    sequence(:sleeve_length, 20)
-    sequence(:sleeve_width, 20)
+    neck { "20" }
+    shoulder_width { "20" }
+    body_width { "20" }
+    body_length { "20" }
+    sleeve_length { "20" }
+    sleeve_width { "20" }
     association :user
   end
 end
--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryBot.define do
   factory :user do
-    sequence(:email) { |n| "user_#{n}@example.com" }
+    email { "user@example.com" }
     password { "password" }
     password_confirmation { "password" }
   end
