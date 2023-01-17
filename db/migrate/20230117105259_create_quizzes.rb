e4ddf5ee8fbf37e5f9276e6af3d9a1b9bff08a75
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2023-01-17 20:28:50 +0900

    [add]add quiz model and migration file

---
+++ db/migrate/20230117105259_create_quizzes.rb
@@ -0,0 +1,15 @@
+class CreateQuizzes < ActiveRecord::Migration[7.0]
+  def change
+    create_table :quizzes do |t|
+      t.string :question, null: false
+      t.string :choice_1, null: false
+      t.string :choice_2, null: false
+      t.string :choice_3, null: false
+      t.string :choice_4, null: false
+      t.string :answer, null: false
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
