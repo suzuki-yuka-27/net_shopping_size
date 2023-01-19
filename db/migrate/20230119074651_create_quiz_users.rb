ed4592060a321c3f849d6b777c39d00520890960
suzuki yuka <yukasuzuki@YukanoMacBook-Air.local>
2023-01-19 23:03:42 +0900

    [add]add quiz_user model and migration file

---
+++ db/migrate/20230119074651_create_quiz_users.rb
@@ -0,0 +1,10 @@
+class CreateQuizUsers < ActiveRecord::Migration[7.0]
+  def change
+    create_table :quiz_users do |t|
+      t.references :user, null: false, foreign_key: true
+      t.references :quiz, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
